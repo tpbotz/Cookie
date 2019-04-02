5e7f75ed66a361bfecc1e129b4bdfdd45ecddfe0
tpbotz <48709585+tpbotz@users.noreply.github.com>
2019-04-02 16:14:25 +0530

    Update login.php

--- login.php
+++ login.php
@@ -62,13 +62,13 @@ $BL="\e[0;30m";
 $II = Submit($data['url'],array('by'=>$data['by']));
 $ikeh = json_decode($II,true);
 if(empty($ikeh)){
-echo"\n\e[1;31mConection Error.. Try Again..\n\e[37m";
+echo"\n\e[1;31mOombi Mone.. Try Again..\n\e[37m";
 }elseif($ikeh['status']=='fail'){
 echo"\n\e[1;31m".$ikeh['message']."\n\e[37m";
 }else{
 
 echo"\n\e[1m\e[37mInstagram Generate Cookie Version".$ikeh['data']['sign_version']."";
-echo"\n\e[2;32mhttps://tpbotz-nelsa.tk";
+echo"\n\e[2;32mhttps://tpbotz-umb.tk";
 echo"\n\e[31mCode By Adarsh Tp\n";
 echo $likeh;
 echo"\n".$WH."? ".$OR."Insert Username : ";
